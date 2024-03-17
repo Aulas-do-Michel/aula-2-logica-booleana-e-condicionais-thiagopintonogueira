d982ee1192ef201d0f488a16b5c7da47411c14eb
Michel Chieregato <michel.chieregato@gmail.com>
2024-03-17 20:42:09 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -1,7 +1,7 @@
 """
 #### Exercício 2 - Identificar se a variante está no gene BRCA1 - Versão 1.
 
-Receba 3 inputs do usuário:
+Receba 2 inputs do usuário:
 1) O cromossomo de uma variante. Ele virá escrito como texto e da seguinte forma "chr1", "chr2", etc.
 2) A posição dessa variante. Será um número inteiro.
 
@@ -10,6 +10,8 @@ Responde:
 "Não" se ela não estiver.
 
 Considere que a variante está no gene BRCA1 se estiver no cromossomo 17 (chr17), e se a posição estiver no intevalo de 41196312 a 41277500.
+
+Obs: Tirei a localização daqui: https://grch37.ensembl.org/Homo_sapiens/Gene/Summary?g=ENSG00000012048;r=17:41196312-41277500.
 
 Exemplos:
 
