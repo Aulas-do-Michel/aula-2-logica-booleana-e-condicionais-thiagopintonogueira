15e4080b61c12e85f80e5630e0cbc7d277a58cfc
Michel Chieregato <michel.chieregato@gmail.com>
2024-03-17 10:34:24 -0300

    Initial commit

---
+++ exercicio_1.py
@@ -0,0 +1,15 @@
+"""
+#### Exercício
+
+Dado um número digitado pelo usuário, imprima seu antecessor, seu sucessor, seu dobro e sua raiz quadrada
+
+Exemplo:
+Digite seu número: 4
+
+Resposta:
+
+O antecessor do número 4 é 3.
+O sucessor do número 4 é 5.
+O dobro do número 4 é 8.
+A raiz quadrada do número 4 é 2.
+"""
